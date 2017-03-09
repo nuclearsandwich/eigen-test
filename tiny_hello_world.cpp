1ae88f0472eda664c59467d1e40e181245795dd1
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-03-09 13:21:02 -0800

    Eigen / TinyXML test repo from Windows worker.

---
+++ tiny_hello_world.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include "tinyxml.h"
+
+void dump_to_stdout( TiXmlNode* pParent, unsigned int indent = 0 )
+{
+	if ( !pParent ) return;
+
+	TiXmlNode* pChild;
+	TiXmlText* pText;
+	int t = pParent->Type();
+	int num;
+
+	switch ( t )
+	{
+	case TiXmlNode::TINYXML_DOCUMENT:
+		printf( "Document" );
+		break;
+
+	case TiXmlNode::TINYXML_ELEMENT:
+		printf( "Element [%s]", pParent->Value() );
+		switch(num)
+		{
+			case 0:  printf( " (No attributes)"); break;
+		}
+		break;
+
+	case TiXmlNode::TINYXML_COMMENT:
+		printf( "Comment: [%s]", pParent->Value());
+		break;
+
+	case TiXmlNode::TINYXML_UNKNOWN:
+		printf( "Unknown" );
+		break;
+
+	case TiXmlNode::TINYXML_TEXT:
+		pText = pParent->ToText();
+		printf( "Text: [%s]", pText->Value() );
+		break;
+
+	case TiXmlNode::TINYXML_DECLARATION:
+		printf( "Declaration" );
+		break;
+	default:
+		break;
+	}
+	printf( "\n" );
+	for ( pChild = pParent->FirstChild(); pChild != 0; pChild = pChild->NextSibling()) 
+	{
+		dump_to_stdout( pChild, indent+1 );
+	}
+}
+
+// load the named file and dump its structure to STDOUT
+void dump_to_stdout(const char* pFilename)
+{
+	TiXmlDocument doc(pFilename);
+	bool loadOkay = doc.LoadFile();
+	if (loadOkay)
+	{
+		printf("\n%s:\n", pFilename);
+		dump_to_stdout( &doc ); // defined later in the tutorial
+	}
+	else
+	{
+		printf("Failed to load file \"%s\"\n", pFilename);
+	}
+}
+
+int main()
+{
+	dump_to_stdout("../demo.xml");
+	return 0;
+}
